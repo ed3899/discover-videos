d6c818d164bf4bf53f41d530df518dcdea868599
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-29 15:49:25 -0500

    198 - nav bar router

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,3 +1,9 @@
+//% libs
+import {useRouter} from "next/router";
+import Link from "next/link";
+
+import {ReactEventHandler} from "react";
+
 //% styles
 import styles from "./navbar.module.css";
 
@@ -7,6 +13,18 @@ type NavBarPropsT = {
 const NavBar = (props: Partial<NavBarPropsT>) => {
   const {username = "Username placeholder"} = props;
 
+  const router = useRouter();
+
+  const handleOnClickHome: ReactEventHandler<HTMLLIElement> = e => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList: ReactEventHandler<HTMLLIElement> = e => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -14,8 +32,12 @@ const NavBar = (props: Partial<NavBarPropsT>) => {
           <div className={styles.logoWrapper}>Netflix</div>
         </a>
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My List</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
@@ -26,7 +48,9 @@ const NavBar = (props: Partial<NavBarPropsT>) => {
 
             <div className={styles.navDropdown}>
               <div>
-                <a className={styles.linkName}>Sign out</a>
+                <Link href="/login">
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
                 <div className={styles.lineWrapper}></div>
               </div>
             </div>
