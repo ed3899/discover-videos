504882a5c3b51a99646e6e5fe0618b7982d4862f
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-29 13:53:54 -0500

    193 - banner component

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,5 +1,33 @@
-const Banner = () => {
-  return <div>Hi from banner</div>;
+type BannerPropsT = {
+  title: string;
+  subTitle: string;
+  imgUrl: string;
+};
+const Banner = (_props: Partial<BannerPropsT>) => {
+  const {
+    title = "placeholder title",
+    subTitle = "placeholder subTitle",
+    imgUrl = "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?v=1530129081",
+  } = _props;
+
+  const handleOnPlay = () => console.log("Handle on play");
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h3>{subTitle}</h3>
+      <button onClick={handleOnPlay}>Play</button>
+      <div
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundSize: "cover",
+          backgroundPosition: "50% 50%",
+        }}></div>
+    </div>
+  );
 };
 
 export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 //% libs
 import type {NextPage} from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 //% comps
 import Banner from "../components/banner";
@@ -21,7 +20,11 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1>Netflix</h1>
 
-        <Banner />
+        <Banner
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
+        />
 
         {/* <NavBar />
         <Card/> */}
