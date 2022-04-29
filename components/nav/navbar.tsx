1060af1db8fe4b3dbfea8019b60978495cd741af
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-29 15:36:11 -0500

    minor css fix

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,3 +1,6 @@
+//% styles
+import styles from "./navbar.module.css";
+
 type NavBarPropsT = {
   username: string;
 };
@@ -5,24 +8,31 @@ const NavBar = (props: Partial<NavBarPropsT>) => {
   const {username = "Username placeholder"} = props;
 
   return (
-    <div>
-      Nav Bar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
-
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
           <div>
-            <a>Sign out</a>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+              {/* Expand more icon */}
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
