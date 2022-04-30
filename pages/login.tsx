d582916fef43fdd0d4bdf0cc42ea9769363af638
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-30 16:17:13 -0500

    229 - setup of signin page

---
+++ pages/login.tsx
@@ -0,0 +1,16 @@
+//% libs
+import type {NextPage} from "next";
+import Head from "next/head";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      Login
+    </div>
+  );
+};
+
+export default Login;
