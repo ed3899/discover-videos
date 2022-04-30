919bae5cd1365d3cb028c5774623dc74bd24c0ce
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-30 16:22:28 -0500

    230 - sign in header component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,10 @@
 //% libs
 import type {NextPage} from "next";
 import Head from "next/head";
+import Image from "next/image";
+
+//% styles
+import styles from "../styles/Login.module.css";
 
 const Login: NextPage = () => {
   return (
@@ -8,7 +12,21 @@ const Login: NextPage = () => {
       <Head>
         <title>Netflix SignIn</title>
       </Head>
-      Login
+
+      <header>
+        <div className={styles.headerWrapper}>
+          <a className={styles.logoLink} href="/">
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix logo"
+                width="128px"
+                height="34px"
+              />
+            </div>
+          </a>
+        </div>
+      </header>
     </div>
   );
 };
