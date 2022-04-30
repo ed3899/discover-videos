62d341288b76dc07165fd16e3fe2f25c64c0ca14
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-30 08:36:20 -0500

    203 - card comp size

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -3,16 +3,33 @@ import Image from "next/image";
 
 import {placeholderImg} from "../../utils";
 
+//% styles
+import styles from "./card.module.css";
+
 type CardPropsT = {
   imgUrl: string;
   size: "large" | "small" | "medium";
 };
 const Card = (props: Partial<CardPropsT>) => {
   const {imgUrl = placeholderImg(), size} = props;
+
+  const classMap: Record<CardPropsT["size"], string> = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       Card
-      <Image src={imgUrl} alt="unknown" width="300px" height="300px" />
+      <div className={size ? classMap[size] : classMap["small"]}>
+        <Image
+          src={imgUrl}
+          alt="unknown"
+          layout="fill"
+          className={styles.cardImg}
+        />
+      </div>
     </div>
   );
 };
