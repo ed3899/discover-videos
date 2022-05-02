ac340f62d8bf28136b842f4bdb8577e76492a96d
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-05-02 12:00:32 -0500

    modal component styles

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -1,12 +1,30 @@
 //% libs
 import {useRouter} from "next/router";
 
+import Modal from "react-modal";
+
+//% styles
+import styles from "../../styles/Video.module.css";
+
 //% types
 import type {NextPage} from "next";
 
+Modal.setAppElement("#__next");
+
 const Video: NextPage = () => {
   const router = useRouter();
-  return <div>Video page</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Example Modal"
+        onRequestClose={() => router.back()}
+        className={styles.modal}
+        overlayClassName={styles.overlay}>
+        <div>Modal body</div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Video;
