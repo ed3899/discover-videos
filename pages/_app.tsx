91511a2ee82c0a8319994ebfdb9a18e3b48f625b
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-05-02 10:17:11 -0500

    app route once logged in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//%
+import {useRouter} from "next/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {useEffect} from "react";
+
+import {magic} from "../lib/magic-client";
+
+//% styles
+import "../styles/globals.css";
+
+//% types
+import type {AppProps} from "next/app";
+
+function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirect = async () => {
+      const isLoggedIn = await magic!.user.isLoggedIn();
+
+      if (isLoggedIn) {
+        router.push("/");
+      } else {
+        router.push("/login");
+      }
+    };
+
+    redirect();
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
